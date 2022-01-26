13abe7e8c2555abab8fc70618f3d18e7a6e0cfd1
unit06 <unit06>
2022-01-27 07:45:34 +0900

    add some text

--- Day 2/tip_calculator.py
+++ Day 2/tip_calculator.py
@@ -1,3 +1,9 @@
 # This is a simple Python project day 2.
 # Tip Calculator
 
+# Welcome to the tip calculator.
+# What was the total bill?
+# How many people to split the bill?
+# What Percentage tip would you like to give? 10, 12, or 15?
+# Each person should pay:
+
