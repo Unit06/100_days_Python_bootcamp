5d6e13bf742a0a0b046345b4cb596c60b9eba48f
Airat <unit29a@hotmail.com>
2022-02-08 17:33:36 +0900

    start coding day 7 solution

--- day7_hangman.py
+++ day7_hangman.py
@@ -1,1 +1,23 @@
 # Hangman day 6 project
+import random
+
+word_list = ["ardvark", "baboon", "camel"]
+#word = word_list[random.randint(0,2)]
+word = random.choice(word_list)
+
+print(f"Pssst, the solution is {word}.")
+
+display = []
+
+for i in range(len(word)):
+    display.append('_')
+
+print(display)
+
+#guess = input("Guess a letter: ")
+
+#for i in word:
+#    if i == guess:
+#        print("Right")
+#    else:
+#        print("Wrong")
