467d26984ad6118bbe1c6a9b16cfc6eeb51c2af3
Airat <unit29a@hotmail.com>
2022-02-28 08:01:51 +0900

    over day 6 project

---
+++ day8_caesar_cipher.py
@@ -0,0 +1,22 @@
+# Caesar Cipher day 6 project
+
+# def greet():
+#     print("Hello ")
+#     print("my ")
+#     print("world!")
+#
+# def greet2(name):
+#     print(f"Hello, {name}.")
+#     print(f"How do you do {name}?")
+#     print("Isn't the weather nice today?")
+#
+# greet()
+# greet2("Airat")
+
+def greet_with(name, location):
+    print(f"Hello, {name}.")
+    print(f"How do you do {name}?")
+    print(f"Isn't the weather in {location} nice today?")
+
+
+greet_with(location="Japan", name="Airat")
