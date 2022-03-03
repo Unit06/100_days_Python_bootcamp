41378f0e43676a391f03bff9606e7d35b7ce054e
Airat <unit29a@hotmail.com>
2022-03-03 17:02:36 +0900

    coding day 8 solution

--- day8_caesar_cipher.py
+++ day8_caesar_cipher.py
@@ -1,22 +1,34 @@
-# Caesar Cipher day 6 project
+# Caesar Cipher day 8 project
+import day7_art
 
-# def greet():
-#     print("Hello ")
-#     print("my ")
-#     print("world!")
-#
-# def greet2(name):
-#     print(f"Hello, {name}.")
-#     print(f"How do you do {name}?")
-#     print("Isn't the weather nice today?")
-#
-# greet()
-# greet2("Airat")
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+temp_text = []
 
-def greet_with(name, location):
-    print(f"Hello, {name}.")
-    print(f"How do you do {name}?")
-    print(f"Isn't the weather in {location} nice today?")
+print(day7_art.logo2)
+direction = input("Type '1' to encrypt, type '2' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
 
 
-greet_with(location="Japan", name="Airat")
+def encrypt(raw_text, user_shift):
+    for i in raw_text:
+        if (alphabet.index(i)+user_shift+1) > len(alphabet):
+            temp_text.append(alphabet[(alphabet.index(i) + user_shift) % (len(alphabet))])
+        else:
+            temp_text.append(alphabet[alphabet.index(i) + user_shift])
+    print("The encoded text is ", ''.join(temp_text))
+
+
+def decrypt(raw_text, user_shift):
+    for i in raw_text:
+        if (alphabet.index(i)-user_shift) < 0:
+            temp_text.append(alphabet[(alphabet.index(i) - user_shift) % (len(alphabet))])
+        else:
+            temp_text.append(alphabet[alphabet.index(i) - user_shift])
+    print("The decoded text is ", ''.join(temp_text))
+
+
+if direction == "1":
+    encrypt(text, shift)
+if direction == "2":
+    decrypt(text, shift)
